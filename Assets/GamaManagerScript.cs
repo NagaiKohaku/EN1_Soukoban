3d6212429e10ffbd5ede9d41d4210b7596c553bc
NagaiKohaku <k023g0037@g.neec.ac.jp>
2024-04-15 13:02:48 +0900

    スライド33まで追加

--- Assets/GamaManagerScript.cs
+++ Assets/GamaManagerScript.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class GamaManagerScript : MonoBehaviour
 {
+    //配列の宣言
+    int[] map;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //配列の実態の作成と初期化
+        map = new int[] { 0, 0, 0, 1, 0, 0, 0, 0, 0 };
+
+        //文字列の宣言と初期化
+        string debugText = "";
+
+        for (int i = 0;i < map.Length; i++)
+        {
+            debugText += map[i].ToString() + ",";
+        }
+
+        //要素数を一つずつ出力
+        Debug.Log(debugText);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            //見つからなかった時のために-1で初期化
+            int playerIndex = -1;
+
+            //要素数はmap.Lengthで取得
+            for(int i = 0;i < map.Length; i++)
+            {
+                if (map[i] == 1)
+                {
+                    playerIndex = i;
+                    break;
+                }
+            }
+        }
     }
 }
